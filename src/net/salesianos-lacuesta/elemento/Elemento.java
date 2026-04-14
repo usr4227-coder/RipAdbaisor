0ea2a9693fb9d61203f4524d66f7126033153194
Nick <usr4227@salesianos-lacuesta.net>
2026-04-14 13:07:13 +0100

    feat: Constructores

--- src/net/salesianos-lacuesta/elemento/Elemento.java
+++ src/net/salesianos-lacuesta/elemento/Elemento.java
@@ -3,4 +3,12 @@ public class Elemento {
     private String categoria;
     private String descripcion;
     private int puntuacion;
+
+ public Elemento (String nombre, String categoria, String descripcion String puntuacion ) {
+    this.nombre = nombre;
+    this.categoria = categoria;
+    this.descripcion = descripcion;
+    this.puntuacion = puntuacion;
+
+ }
 }
