c73c57a045efe2958065e6a939c5556ece685729
Nick <usr4227@salesianos-lacuesta.net>
2026-04-14 13:30:45 +0100

    Getters y Setters

--- src/net/salesianos-lacuesta/elemento/Elemento.java
+++ src/net/salesianos-lacuesta/elemento/Elemento.java
@@ -9,6 +9,38 @@ public class Elemento {
     this.categoria = categoria;
     this.descripcion = descripcion;
     this.puntuacion = puntuacion;
+    
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
 
+     public String getCategoria() {
+        return categoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+     public int getPuntuacion() {
+        return puntuacion;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+    
  }
 }
