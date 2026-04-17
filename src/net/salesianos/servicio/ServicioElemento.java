e1340d3fd5f91cef2e56c79ef2dd198b68d96f93
Nick <usr4227@salesianos-lacuesta.net>
2026-04-17 10:43:31 +0100

    feat: Crear Servicio Elementos

--- src/net/salesianos/servicio/ServicioElemento.java
+++ src/net/salesianos/servicio/ServicioElemento.java
@@ -1,5 +1,36 @@
 package net.salesianos.servicio;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import net.salesianos
+import net.salesianos.elemento.Elemento;
+import net.salesianos.elemento.Elemento;
+import net.salesianos.lacuesta.elemento.Elemento;
+
 public class ServicioElemento {
- private String nombre;
+    rivate List<Elemento>elementos;
+
+public ServicioElementos() {
+        this.elementos = new ArrayList<>();
+    }
+
+    public void agregarElemento(Elemento elemento) {
+        elementos.add(elemento);
+    }
+
+    public void editarElemento(int indice, Elemento nuevoElemento) {
+        if (indice >= 0 && indice < elementos.size()) {
+            elementos.set(indice, nuevoElemento);
+        }
+    }
+
+    public void eliminarElemento(int indice) {
+        if (indice >= 0 && indice < elementos.size()) {
+            elementos.remove(indice);
+        }
+    }
+
 }
