00e81b4b802d2cf8816d45955ad2eed394eec163
Nick <usr4227@salesianos-lacuesta.net>
2026-04-17 14:01:01 +0100

    Actualizar servicio

--- src/net/salesianos/servicio/ServicioElemento.java
+++ src/net/salesianos/servicio/ServicioElemento.java
@@ -2,7 +2,6 @@ package net.salesianoslacuesta.servicio;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import net.salesianoslacuesta.elemento.Elemento;
 
 public class ServicioElementos {
