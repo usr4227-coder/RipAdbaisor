c665392c26516090dd861a90b04faaa3fa6928b4
Nick <usr4227@salesianos-lacuesta.net>
2026-04-20 10:58:04 +0100

    feat: Validador terminado

--- src/net/salesianos/util/Validador.java
+++ src/net/salesianos/util/Validador.java
@@ -10,8 +10,15 @@ public class Validador {
         }
     }
 
-    public static int convertirEnteri(String texto) {
+    public static int convertirEntero(String texto) {
         return Integer.pareseInt(texto);
     }
 
+    public static boolean estaRango(int numero, int min, int max) {
+        return numero >= min && numero <= max;
+    }
+
+    public static boolean textoValidado(String texto) {
+        return texto != null && !texto.trim().isEmpty();
+    }
 }
