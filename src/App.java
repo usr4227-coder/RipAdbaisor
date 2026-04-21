2b1b0f058425f9b36a4730104453ecad7252cdd0
Nick <usr4227@salesianos-lacuesta.net>
2026-04-21 11:15:49 +0100

    App creada

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+
+import net.salesianos.servicio.ServicioElementos;
+import net.salesianos.interfaz.MenuPrincipal;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+
+        ServicioElementos servicio = new ServicioElementos();
+        MenuPrincipal menu = new MenuPrincipal(servicio);
+
+        menu.iniciar();
     }
 }
